16442d90a4742bbf2e08191d864a394b83b6212f
John O'B <john.f.obrienjr@gmail.com>
2020-05-29 06:58:49 -0400

    Redefining things for ESP32

--- esp32-6050-datacollector/esp32-6050-datacollector.ino
+++ esp32-6050-datacollector/esp32-6050-datacollector.ino
@@ -1,69 +1,138 @@
+#include "I2Cdev.h"
+#include "MPU6050_6Axis_MotionApps20.h"
+#include "Wire.h"
 #include <SPI.h>
 #include <SD.h>
-#include <Wire.h>
 #include <TinyGPS++.h>
 #include <SoftwareSerial.h>
 
+MPU6050 mpu;
 
-//MPU constants
-const int MPU_addr = 0x68; // I2C address of the MPU-6050
-int16_t AcX, AcY, AcZ, Tmp, GyX, GyY, GyZ;
-int LED_BUILTIN = 2;
 
-// Set the pins used
-#define cardSelect 5
+#define INTERRUPT_PIN 15  // use pin 2 on Arduino Uno & most boards
+#define LED_PIN 2 // (Arduino is 13, Teensy is 11, Teensy++ is 6)
+bool blinkState = false;
+
+
+// Set the variables for SD Cards
+#define cardSelect 5 //SD Card pin select
+File logfile;
+char filename[15];
 
 // Software serial for GPS
 // Connect the GPS TX (transmit) pin to Digital 4
 // Connect the GPS RX (receive) pin to Digital 2
 static const int RXPin = 4, TXPin = 2;
 static const uint32_t GPSBaud = 9600;
+
 // The TinyGPS++ object
 TinyGPSPlus gps;
 
 // The serial connection to the GPS device
 SoftwareSerial ss(RXPin, TXPin);
 
-// blink out an error code
-void error(uint8_t errno) 
-{
-  while (1) {
-    uint8_t i;
-    for (i = 0; i < errno; i++) {
-      digitalWrite(13, HIGH);
-      delay(100);
-      digitalWrite(13, LOW);
-      delay(100);
-    }
-    for (i = errno; i < 10; i++) {
-      delay(200);
-    }
-  }
+
+// MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;    // [x, y, z]            gravity vector
+
+
+// ================================================================
+// ===               INTERRUPT DETECTION ROUTINE                ===
+// ================================================================
+
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+
+void dmpDataReady() {
+  mpuInterrupt = true;
 }
 
+// ================================================================
+// ===                      INITIAL SETUP                       ===
+// ================================================================
 
-char filename[15];
+void setup() {
+  // join I2C bus (I2Cdev library doesn't do this automatically)
+  Wire.begin(21, 22, 400000);
+  Wire.setClock(400000);
 
-void setup() 
-{
-  // connect at 115200 so we can read the GPS fast enough and echo without dropping chars
-  // also spit it out
-  Serial.begin(115200);
-  Serial.println("\r\nAnalog logger test");
   pinMode(13, OUTPUT);
-  init6050();
   initGPS();
-  //
-  pinMode (LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(1000);
-  // see if the card is present and can be initialized:
+
+  Serial.begin(115200);
+
+  // initialize device
+  Serial.println(F("Initializing I2C devices..."));
+  delay(500);
+  mpu.initialize();
+  delay(500);
+
+  // verify connection
+  Serial.println(F("Testing device connections..."));
+  Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+
+  // load and configure the DMP
+  Serial.println(F("Initializing DMP..."));
+  delay(500);
+  devStatus = mpu.dmpInitialize();
+  delay(500);
+
+  // supply your own gyro offsets here, scaled for min sensitivity
+  mpu.setXGyroOffset(79);
+  mpu.setYGyroOffset(65);
+  mpu.setZGyroOffset(19);
+  mpu.setXAccelOffset(-3011);
+  mpu.setYAccelOffset(-1833);
+  mpu.setZAccelOffset(1030); // 1688 factory default for my test chip
+
+  // make sure it worked (returns 0 if so)
+  if (devStatus == 0) {
+    // Calibration Time: generate offsets and calibrate our MPU6050
+    mpu.CalibrateAccel(6);
+    mpu.CalibrateGyro(6);
+    mpu.PrintActiveOffsets();
+    // turn on the DMP, now that it's ready
+    Serial.println(F("Enabling DMP..."));
+    mpu.setDMPEnabled(true);
+
+
+    pinMode(INTERRUPT_PIN, INPUT);
+    attachInterrupt(INTERRUPT_PIN, dmpDataReady, RISING);
+    mpuIntStatus = mpu.getIntStatus();
+
+    // set our DMP Ready flag so the main loop() function knows it's okay to use it
+    Serial.println(F("DMP ready! Waiting for first interrupt..."));
+    dmpReady = true;
+
+    // get expected DMP packet size for later comparison
+    packetSize = mpu.dmpGetFIFOPacketSize();
+  } else {
+    // ERROR!
+    // 1 = initial memory load failed
+    // 2 = DMP configuration updates failed
+    // (if it's going to break, usually the code will be 1)
+    Serial.print(F("DMP Initialization failed (code "));
+    Serial.print(devStatus);
+    Serial.println(F(")"));
+  }
+
+  // configure LED for output
+  pinMode(LED_PIN, OUTPUT);
+
   if (!SD.begin(cardSelect)) {
     Serial.println("Card init. failed!");
-    digitalWrite(LED_BUILTIN, HIGH);
-    error(2);
+    digitalWrite(LED_PIN, HIGH);
   }
   strcpy(filename, "/ANALOG00.TXT");
   for (uint8_t i = 0; i < 100; i++) {
@@ -79,7 +148,6 @@ void setup()
   if ( ! logfile ) {
     Serial.print("Couldnt create ");
     Serial.println(filename);
-    error(3);
   }
   logfile.close();
   Serial.print("Writing to ");
@@ -87,42 +155,52 @@ void setup()
 
   pinMode(8, OUTPUT);
   Serial.println("Ready!");
+
 }
 
-void getAccels()
-{
-  Wire.beginTransmission(MPU_addr);
-  Wire.write(0x3B); // starting with register 0x3B (ACCEL_XOUT_H)
-  Wire.endTransmission(false);
-  Wire.requestFrom(MPU_addr, 14, true); // request a total of 14 registers
 
-  AcX = Wire.read() << 8 | Wire.read(); // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)
-  AcY = Wire.read() << 8 | Wire.read(); // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
-  AcZ = Wire.read() << 8 | Wire.read(); // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
-  Tmp = Wire.read() << 8 | Wire.read(); // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
-  GyX = Wire.read() << 8 | Wire.read(); // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
-  GyY = Wire.read() << 8 | Wire.read(); // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
-  GyZ = Wire.read() << 8 | Wire.read(); // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
-}
 
-void loop() 
-{
-  digitalWrite(8, HIGH);
-  getAccels();
-  Serial.print("AcX = "); Serial.print(AcX);
-  Serial.print(" | AcY = "); Serial.print(AcY);
-  Serial.print(" | AcZ = "); Serial.print(AcZ); Serial.print(",");
-  File logfile;
-  logfile = SD.open(filename, FILE_APPEND);
-  logfile.print(AcX); logfile.print(",");
-  logfile.print(AcY); logfile.print(",");
-  logfile.print(AcZ); logfile.print(",");
+// ================================================================
+// ===                    MAIN PROGRAM LOOP                     ===
+// ================================================================
 
-  while (ss.available() > 0)
-  {
-    gps.encode(ss.read());
+void loop() {
+  // if programming failed, don't try to do anything
+  if (!dmpReady) return;
+  // read a packet from FIFO
+  if (mpu.dmpGetCurrentFIFOPacket(fifoBuffer)) { // Get the Latest packet
+    mpu.dmpGetQuaternion(&q, fifoBuffer);
+    mpu.dmpGetAccel(&aa, fifoBuffer);
+    mpu.dmpGetGravity(&gravity, &q);
+    mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
+
+    logfile = SD.open(filename, FILE_APPEND);
+    logfile.print(millis()); logfile.print(",");// Raw time in HHMMSSCC format (u32)
+    logfile.print(gps.time.value()); logfile.print(",");// Raw time in HHMMSSCC format (u32)
+    logfile.print(aaReal.x); logfile.print(",");
+    logfile.print(aaReal.y); logfile.print(",");
+    logfile.print(aaReal.z); logfile.print(",");
+    while (ss.available() > 0)
+    {
+      gps.encode(ss.read());
+    }
+    gpsWrite();
+    Serial.print(gps.time.value()); Serial.print(",");// Raw time in HHMMSSCC format (u32)
+    Serial.print(aaReal.x); Serial.print(",");
+    Serial.print(aaReal.y); Serial.print(",");
+    Serial.print(aaReal.z); Serial.print(",");
+
+    logfile.println(",");
+    Serial.println(",");
+
+    // blink LED to indicate activity
+    blinkState = !blinkState;
+    digitalWrite(LED_PIN, blinkState);
   }
+}
 
+void gpsWrite()
+{
   logfile.print(gps.location.lat(), 6);
   logfile.print(",");
   logfile.print(gps.location.lng(), 6);
@@ -138,26 +216,9 @@ void loop()
   Serial.print(gps.speed.value());
   Serial.print(",");
   Serial.print(gps.course.deg());
-  Serial.println(",");
-
-  logfile.close();
-  digitalWrite(8, LOW);
-
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(10);
-  digitalWrite(LED_BUILTIN, HIGH);
 }
 
-void init6050() 
-{
-  Wire.begin();
-  Wire.beginTransmission(MPU_addr);
-  Wire.write(0x6B); // PWR_MGMT_1 register
-  Wire.write(0); // set to zero (wakes up the MPU-6050)
-  Wire.endTransmission(true);
-}
-
-void initGPS() 
+void initGPS()
 {
   ss.begin(GPSBaud);
   delay(1000);
