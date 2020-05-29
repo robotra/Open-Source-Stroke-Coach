52266baee6c191e8aff8444735deccc84dae9eb6
John O'B <john.f.obrienjr@gmail.com>
2020-05-29 07:26:19 -0400

    Test commit from vscode

--- esp32-6050-datacollector/esp32-6050-datacollector.ino
+++ esp32-6050-datacollector/esp32-6050-datacollector.ino
@@ -8,7 +8,7 @@
 
 MPU6050 mpu;
 
-
+//test commit 
 #define INTERRUPT_PIN 15  // use pin 2 on Arduino Uno & most boards
 #define LED_PIN 2 // (Arduino is 13, Teensy is 11, Teensy++ is 6)
 bool blinkState = false;
